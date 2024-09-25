bb056653b8a3fac58804c56c2dbec2d4877b1a7c
balidani <balijanosdaniel@gmail.com>
2024-09-25 20:34:58 +0200

    rework reloading

--- game_settings.js
+++ game_settings.js
@@ -1,70 +1,77 @@
 import * as Utils from "./util.js"
 
 export class GameSettings {
-    static loadFn;
+  static loadFn;
 
-    constructor() {
-        this.settingsDiv = document.querySelector('.game .settings');
-        this.settingsOpener = document.querySelector('.open-settings');
+  static settings = null;
+  static instance() {
+    if (GameSettings.settings === null) {
+      GameSettings.settings = new GameSettings();
+    }
+    return GameSettings.settings;
+  }
 
-        this.settingsOpener.addEventListener('click', (_) => {
-            this.open()
-        })
+  constructor() {
+    this.settingsDiv = document.querySelector('.game .settings');
+    this.settingsOpener = document.querySelector('.open-settings');
 
-        this.isOpen = false;
-        this.boardX = 5;
-        this.boardY = 5;
-        this.startingSet = "🍒🍒🍒🪙🍀"
-        this.symbolSources = ['./symbol.js']
-        // , './extra_sym.js'
+    this.settingsOpener.addEventListener('click', (_) => {
+      this.open()
+    })
+
+    this.isOpen = false;
+    this.boardX = 5;
+    this.boardY = 5;
+    this.startingSet = "🍒🍒🍒🪙🍀"
+    this.symbolSources = ['./symbol.js']
+    // , './extra_sym.js'
+  }
+
+  async open(game) {
+    if (this.isOpen) {
+      return;
+    }
+    this.isOpen = true;
+    this.settingsDiv = document.querySelector('.game .settings');
+    this.settingsDiv.replaceChildren();
+
+    // Create input elements
+    const numRowsInput = Utils.createInput('# of Rows', 'number', this.boardX);
+    const numColumnsInput = Utils.createInput('# of Columns', 'number', this.boardY);
+    const symbolSourcesInput = Utils.createInput('Symbol Sources', 'textarea', this.symbolSources.join("\n"));
+    const startingSymbolsInput = Utils.createInput('Starting Symbols', 'text', this.startingSet);
+    // Create buttons
+    const cancelButton = Utils.createButton('Cancel', async () => await this.close());
+    const saveButton = Utils.createButton('Save', () => this.save(numRowsInput, numColumnsInput, symbolSourcesInput, startingSymbolsInput));
+
+    // Append elements to the settings div
+    const settingsBoxDiv = document.createElement('div');
+    settingsBoxDiv.classList.add('settings-box')
+    settingsBoxDiv.append(numRowsInput.label, numRowsInput.input);
+    settingsBoxDiv.append(numColumnsInput.label, numColumnsInput.input);
+    settingsBoxDiv.append(symbolSourcesInput.label, symbolSourcesInput.input);
+    settingsBoxDiv.append(startingSymbolsInput.label, startingSymbolsInput.input);
+    settingsBoxDiv.append(cancelButton, saveButton);
+    
+    this.settingsDiv = document.querySelector('.game .settings');
+    this.settingsDiv.append(settingsBoxDiv);
+  }
+
+  async close(game) {
+    if (!this.isOpen) {
+      return;
     }
 
-    async open(game) {
-        if (this.isOpen) {
-            return;
-        }
-        this.isOpen = true;
-        this.settingsDiv = document.querySelector('.game .settings');
-        this.settingsDiv.replaceChildren();
+    this.settingsDiv.replaceChildren();
+    this.isOpen = false;
+  }
 
-        // Create input elements
-        const numRowsInput = Utils.createInput('# of Rows', 'number', this.boardX);
-        const numColumnsInput = Utils.createInput('# of Columns', 'number', this.boardY);
-        const symbolSourcesInput = Utils.createInput('Symbol Sources', 'textarea', this.symbolSources.join("\n"));
-        const startingSymbolsInput = Utils.createInput('Starting Symbols', 'text', this.startingSet);
-        // Create buttons
-        const cancelButton = Utils.createButton('Cancel', async () => await this.close());
-        const saveButton = Utils.createButton('Save', () => this.save(numRowsInput, numColumnsInput, symbolSourcesInput, startingSymbolsInput));
-
-        // Append elements to the settings div
-        const settingsBoxDiv = document.createElement('div');
-        settingsBoxDiv.classList.add('settings-box')
-        settingsBoxDiv.append(numRowsInput.label, numRowsInput.input);
-        settingsBoxDiv.append(numColumnsInput.label, numColumnsInput.input);
-        settingsBoxDiv.append(symbolSourcesInput.label, symbolSourcesInput.input);
-        settingsBoxDiv.append(startingSymbolsInput.label, startingSymbolsInput.input);
-        settingsBoxDiv.append(cancelButton, saveButton);
-        
-        this.settingsDiv = document.querySelector('.game .settings');
-        this.settingsDiv.append(settingsBoxDiv);
-    }
-
-    async close(game) {
-        if (!this.isOpen) {
-            return;
-        }
-
-        this.settingsDiv.replaceChildren();
-        this.isOpen = false;
-    }
-
-    save(numRowsInput, numColsInput, symbolSourcesInput, startingSymbolsInput) {
-        this.boardX = numRowsInput.input.value;
-        this.boardY = numColsInput.input.value;
-        this.symbolSources = symbolSourcesInput.input.value.split("\n");
-        this.startingSet = startingSymbolsInput.input.value;
-        this.close();
-        GameSettings.loadFn(this);
-    }
+  save(numRowsInput, numColsInput, symbolSourcesInput, startingSymbolsInput) {
+    this.boardX = numRowsInput.input.value;
+    this.boardY = numColsInput.input.value;
+    this.symbolSources = symbolSourcesInput.input.value.split("\n");
+    this.startingSet = startingSymbolsInput.input.value;
+    this.close();
+    GameSettings.loadFn(this);
+  }
 }
-
