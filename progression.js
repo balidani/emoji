6a02a0a67a2c9efd9709f6e132229aa87276891c
balidani <balijanosdaniel@gmail.com>
2024-09-29 18:04:21 +0200

    bump version number to force local storage clearance

--- progression.js
+++ progression.js
@@ -6,7 +6,7 @@ const tutorialLevelSettings = new GameSettings("Tutorial #1", 4, 4, 15, "🍒�
   { 100: '🥇' }, { 50: 'Welcome to the Tutorial!' });
 const standardGameSettings = new GameSettings();
 
-const CURRENT_VERSION = "0.1.2";
+const CURRENT_VERSION = "0.1.3";
 const CURRENT_VERSION_KEY = "CurrentVersion";
 const PROGRESSION_LEVEL_DATA = "ProgressionLevelData";
 const PROGRESSION_ACTIVE_LEVEL = "ProgressionActiveLevel";
